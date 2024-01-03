d2a1961d80e3a242a76645973c86c8b3b03345c6
Henrique <henriquemoreira10fk@gmail.com>
2024-01-03 20:37:01 -0300

    `fix` endpoints url and param verification codes

--- api.py
+++ api.py
@@ -3,18 +3,16 @@ from flask_limiter import Limiter
 from flask_limiter.util import get_remote_address
 from flask_caching import Cache
 from typing import Any
+from re import compile, match
 
-from api_resources.tools_endpoints.v1.url_generator.mediafire import main as url_generator__mediafire
-from api_resources.tools_endpoints.v1.url_generator.googledrive import main as url_generator__googledrive
+from api_resources.tools_endpoints.url_generator.v1.mediafire import main as url_generator__mediafire
+from api_resources.tools_endpoints.url_generator.v1.googledrive import main as url_generator__googledrive
 
-from api_resources.tools_endpoints.v1.wrapper.aliexpress import main as wrapper__aliexpress
+from api_resources.tools_endpoints.wrapper.v1.aliexpress import main as wrapper__aliexpress
 
-from api_resources.tools_endpoints.v1.randomizer.random_int_number import main as randomizer__random_int_number
-from api_resources.tools_endpoints.v1.randomizer.random_float_number import main as randomizer__random_float_number
+from api_resources.tools_endpoints.randomizer.v1.random_int_number import main as randomizer__random_int_number
+from api_resources.tools_endpoints.randomizer.v1.random_float_number import main as randomizer__random_float_number
 
-
-# Flask required variables
-latest_api_version = 'v1'
 
 # Initialize Flask app and your plugins
 app = Flask(__name__)
@@ -45,35 +43,34 @@ def home() -> jsonify:
         'author_github': 'https://github.com/Henrique-Coder',
         'source_code_url': 'https://github.com/Henrique-Coder/everytools-api',
         'base_url': 'http://node1.mindwired.com.br:8452',
-        'latest_api_version': latest_api_version,
         'endpoints': {
             'url-generator': {
                 'mediafire': {
-                    'url': f'/{latest_api_version}/url-generator/mediafire?id=',
+                    'url': f'/url-generator/mediafire?id=',
                     'description': 'Generates a direct download link for items hosted on MediaFire.',
                     'rate_limit': '1/second;30/minute;200/hour;600/day',
                 },
                 'googledrive': {
-                    'url': f'/{latest_api_version}/url-generator/googledrive?id=',
+                    'url': f'/url-generator/googledrive?id=',
                     'description': 'Generates a direct download link for items hosted on Google Drive.',
                     'rate_limit': '1/second;30/minute;200/hour;600/day',
                 }
             },
             'wrapper': {
                 'aliexpress-product': {
-                    'url': f'/{latest_api_version}/wrapper/aliexpress-product?id=',
+                    'url': f'/wrapper/aliexpress-product?id=',
                     'description': 'Wraps AliExpress product info into a friendly JSON format.',
                     'rate_limit': '1/second;30/minute;200/hour;600/day',
                 }
             },
             'randomizer': {
                 'random-int-number': {
-                    'url': f'/{latest_api_version}/randomizer/random-int-number?min=&max=',
+                    'url': f'/randomizer/random-int-number?min=&max=',
                     'description': 'Generates a random integer number between two numbers.',
                     'rate_limit': '5/second;5000/day',
                 },
                 'random-float-number': {
-                    'url': f'/{latest_api_version}/randomizer/random-float-number?min=&max=',
+                    'url': f'/randomizer/random-float-number?min=&max=',
                     'description': 'Generates a random float number between two numbers.',
                     'rate_limit': '5/second;5000/day',
                 }
@@ -83,15 +80,16 @@ def home() -> jsonify:
 
 
 # Flask API routes
+
 # Route: /url-generator/mediafire -> Generates a direct download link for items hosted on MediaFire.
-@app.route(f'/{latest_api_version}/url-generator/mediafire', methods=['GET'])
+@app.route('/url-generator/v1/mediafire', methods=['GET'])
 @limiter.limit('1/second;30/minute;200/hour;600/day')
 @cache.cached(timeout=300, make_cache_key=_make_cache_key)
 def _url_generator__mediafire() -> jsonify:
     p_id = request.args.get('id')
 
     if not p_id or not p_id.isalnum():
-        return jsonify({'success': False, 'message': "The id parameter is required, must exist and must be alphanumeric."}), 400
+        return jsonify({'success': False, 'message': "The id parameter is required and must be alphanumeric."}), 400
 
     output_data = url_generator__mediafire(p_id)
 
@@ -102,14 +100,14 @@ def _url_generator__mediafire() -> jsonify:
 
 
 # Route: /url-generator/googledrive -> Generates a direct download link for items hosted on Google Drive.
-@app.route(f'/{latest_api_version}/url-generator/googledrive', methods=['GET'])
+@app.route('/url-generator/v1/googledrive', methods=['GET'])
 @limiter.limit('1/second;30/minute;200/hour;600/day')
 @cache.cached(timeout=300, make_cache_key=_make_cache_key)
 def _url_generator__googledrive() -> jsonify:
     p_id = request.args.get('id')
 
-    if not p_id or not p_id.isalnum():
-        return jsonify({'success': False, 'message': "The id parameter is required, must exist and must be alphanumeric."}), 400
+    if not p_id or not compile(r'^[a-zA-Z0-9_-]+$').match(p_id):
+        return jsonify({'success': False, 'message': "The id parameter is required and must be alphanumeric."}), 400
 
     output_data = url_generator__googledrive(p_id)
 
@@ -120,14 +118,14 @@ def _url_generator__googledrive() -> jsonify:
 
 
 # Route: /wrapper/aliexpress-product -> Wraps AliExpress product info into a friendly JSON format.
-@app.route(f'/{latest_api_version}/wrapper/aliexpress-product', methods=['GET'])
+@app.route('/wrapper/v1/aliexpress-product', methods=['GET'])
 @limiter.limit('1/second;30/minute;200/hour;600/day')
 @cache.cached(timeout=300, make_cache_key=_make_cache_key)
 def _wrapper__aliexpress_product() -> jsonify:
     p_id = request.args.get('id')
 
     if not p_id or not p_id.isnumeric():
-        return jsonify({'success': False, 'message': "The id parameter is required, must exist and must be numeric."}), 400
+        return jsonify({'success': False, 'message': "The id parameter is required and must be numeric."}), 400
 
     p_id = int(p_id)
     output_data = wrapper__aliexpress(p_id)
@@ -139,16 +137,20 @@ def _wrapper__aliexpress_product() -> jsonify:
 
 
 # Route: /randomizer/random-int-number -> Generates a random integer number between two numbers.
-@app.route(f'/{latest_api_version}/randomizer/random-int-number', methods=['GET'])
+@app.route('/randomizer/v1/random-int-number', methods=['GET'])
 @limiter.limit('5/second;5000/day')
 def _randomizer__random_int_number() -> jsonify:
     p_min = request.args.get('min')
     p_max = request.args.get('max')
 
-    if not p_min or not p_min.isnumeric() or not p_max or not p_max.isnumeric() or p_min > p_max:
-        return jsonify({'success': False, 'message': "The min and max parameters are required, must exist and must be numeric. The min parameter must be less than the max parameter."}), 400
+    if not p_min or not p_min.isnumeric() or not p_max or not p_max.isnumeric():
+        return jsonify({'success': False, 'message': "The min and max parameters are required and must be integers."}), 400
 
-    output_data = randomizer__random_int_number(int(p_min), int(p_max))
+    p_min, p_max = int(p_min), int(p_max)
+    if p_min > p_max:
+        return jsonify({'success': False, 'message': "The min parameter must be less than the max parameter."}), 400
+
+    output_data = randomizer__random_int_number(p_min, p_max)
 
     if output_data:
         return jsonify({'success': True, 'number': output_data, 'type': 'int', 'query': {'min': p_min, 'max': p_max}}), 200
@@ -157,7 +159,7 @@ def _randomizer__random_int_number() -> jsonify:
 
 
 # Route: /randomizer/random-float-number -> Generates a random float number between two numbers.
-@app.route(f'/{latest_api_version}/randomizer/random-float-number', methods=['GET'])
+@app.route('/randomizer/v1/random-float-number', methods=['GET'])
 @limiter.limit('5/second;5000/day')
 def _randomizer__random_float_number() -> jsonify:
     def is_float(value: Any) -> bool:
@@ -170,8 +172,12 @@ def _randomizer__random_float_number() -> jsonify:
     p_min = request.args.get('min')
     p_max = request.args.get('max')
 
-    if not p_min or not is_float(p_min) or not p_max or not is_float(p_max) or float(p_min) > float(p_max):
-        return jsonify({'success': False, 'message': "The min and max parameters are required, must exist and must be integer or float numbers. The min parameter must be less than the max parameter."}), 400
+    if not p_min or not is_float(p_min) or not p_max or not is_float(p_max):
+        return jsonify({'success': False, 'message': "The min and max parameters are required and must be floats."}), 400
+
+    p_min, p_max = float(p_min), float(p_max)
+    if p_min > p_max:
+        return jsonify({'success': False, 'message': "The min parameter must be less than the max parameter."}), 400
 
     output_data = randomizer__random_float_number(float(p_min), float(p_max))
 
@@ -182,4 +188,4 @@ def _randomizer__random_float_number() -> jsonify:
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', threaded=True, port=8452)
+    app.run(debug=False, host='0.0.0.0', threaded=True, port=8452)
