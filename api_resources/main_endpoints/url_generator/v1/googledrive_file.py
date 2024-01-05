a27a20e92a86d16f97362df4df764d5070d8a750
Henrique <henriquemoreira10fk@gmail.com>
2024-01-05 09:24:49 -0300

    `change` request to httpx

--- api_resources/main_endpoints/url_generator/v1/googledrive_file.py
+++ api_resources/main_endpoints/url_generator/v1/googledrive_file.py
@@ -1,11 +1,11 @@
 from typing import Union
-from requests import get as requests_get
+from httpx import get as httpx_get
 from lxml import html
 
 
 def main(_id: str) -> Union[str, None]:
     url = f'https://drive.google.com/uc?export=download&id={_id}'
-    resp = requests_get(url, allow_redirects=False, timeout=5)
+    resp = httpx_get(url, follow_redirects=False, timeout=5)
 
     try:
         tree = html.fromstring(resp.content)
