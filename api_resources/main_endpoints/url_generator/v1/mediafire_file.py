91b33ddbc221e91d1d7be91ab268a5802a00b238
Henrique <henriquemoreira10fk@gmail.com>
2024-01-05 09:25:13 -0300

    `change` request to httpx and added url var

--- api_resources/main_endpoints/url_generator/v1/mediafire_file.py
+++ api_resources/main_endpoints/url_generator/v1/mediafire_file.py
@@ -1,11 +1,12 @@
 from typing import Union
-from requests import get as requests_get
+from httpx import get as httpx_get
 from lxml import html
 
 
 def main(_id: str) -> Union[str, None]:
     try:
-        resp = requests_get(f'https://www.mediafire.com/file/{_id}', allow_redirects=False, timeout=5)
+        url = f'https://www.mediafire.com/file/{_id}'
+        resp = httpx_get(url, follow_redirects=False, timeout=5)
         tree = html.fromstring(resp.content)
         data = tree.xpath('//a[@id="downloadButton"]/@href')[0]
         data = str(data[:data.rfind('/')])
